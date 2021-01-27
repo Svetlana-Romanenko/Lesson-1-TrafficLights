ffa55c413dd39d595621e7e7118fa237b3f610f9
Svetlana <svetaromanenko@inbox.ru>
2021-01-28 00:34:08 +0300

    Complite task 2

--- Lesson 1 TrafficLights/ViewController.swift
+++ Lesson 1 TrafficLights/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha=0.3
+        yellowLightView.alpha=0.3
+        greenLightView.alpha=0.3
+        
+        redLightView.layer.cornerRadius=56.75
+        yellowLightView.layer.cornerRadius=56.75
+        greenLightView.layer.cornerRadius=56.75
     }
 
-
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle=="START" {
+            redLightView.alpha=1
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        else if redLightView.alpha == 1 {
+            redLightView.alpha=0.3
+            yellowLightView.alpha=1
+        }
+        else if yellowLightView.alpha == 1{
+            yellowLightView.alpha=0.3
+            greenLightView.alpha=1
+        }
+        else {
+            greenLightView.alpha=0.3
+            redLightView.alpha=1
+        }
+    }
+    
 }
 
