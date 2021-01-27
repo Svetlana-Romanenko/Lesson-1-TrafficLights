881c2efcd5e64c7dd5ba3c280cad742a0aa99328
Svetlana <svetaromanenko@inbox.ru>
2021-01-27 22:24:46 +0300

    Initial Commit

---
+++ Lesson 1 TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1 TrafficLights
+//
+//  Created by Светлана Романенко on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
